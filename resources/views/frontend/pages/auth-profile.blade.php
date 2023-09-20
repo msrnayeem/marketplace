7daf7e40577a30d038492621ec0355a8cffd4e50
Msr Nayeem <msrnayeem@gmail.com>
2023-09-20 15:39:42 +0600

    profile

--- resources/views/frontend/pages/auth-profile.blade.php
+++ resources/views/frontend/pages/auth-profile.blade.php
@@ -132,27 +132,73 @@
                 </div>
             </div>
         </div>
-        
-      
-        <!-- right column become seller-->
-        <!-- <div class="col sm-1 col-md-7 col-xl-9 mb-sm-3 p-3 p-md-4 p-xl-4" style="height:400px;">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div> -->
 
-        <!-- right column gigs-->
-        <div class="col sm-1 col-md-7 col-xl-9 mb-sm-3 p-3 p-md-4 p-xl-4 d-none d-sm-block" style="height:400px;">
+        <!-- right column seller-->
+        <!-- <div class="col sm-1 col-md-7 col-xl-9 mb-sm-3 p-3 p-md-4 p-xl-4 d-none d-sm-block" style="height:400px;">
             <div class="card h-100">
                 <div class="card-body">
                     <a href="#" class="btn-standard btn-green rounded">Become Seller</a>
                 </div>
             </div>
+        </div> -->
+      
+        <!-- right column become seller-->
+
+        <div class="col sm-1 col-md-7 col-xl-9 mb-sm-3 p-3 p-md-4 p-xl-4">
+            <div class="container h-100 w-100">
+                <div class="row mx-0" >
+                    <div class="container mt-2 p-0">
+                        <div class="row mx-0" style="height:40px;">
+                            <div class="border border-2 bg-light d-flex">
+                                <div class="flex-grow-1">
+                                    <label class="text-center fs-5" id="active-gigs">Active</label>
+                                </div>
+                                <div class="flex-grow-1">
+                                    <label class="text-center fs-5"  id="drafts">Drafts</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mx-0 h-100 w-100">
+                    <div class="container mt-2 p-0" id="for_gigs">                      
+                        <div class="row">
+                            @for($j=0; $j<10; $j++)
+                            <div class="col-md-6 col-sm-12 mb-3">
+                                <div class="card">
+                                    <img src="{{ asset('image.png') }}" class="card-img-top" alt="image">
+                                    <div class="card-body d-flex justify-content-between">
+                                        <h5 class="card-title" style="cursor: default;">Title Title Title Title Title</h5>
+                                        <p style="font-size: 12px;">Starting at - 5</p>
+                                    </div>
+                                </div>
+                            </div>
+                            @endfor
+                        </div>
+                       
+                    </div>
+
+                    <div class="container mt-2 p-0" id="for_drafts" style="display: none;">
+                        <div class="row">
+                            @for($j=0; $j<5; $j++)
+                            <div class="col-md-6 col-sm-1 mb-3">
+                                <div class="card">
+                                    <img src="{{ asset('image.png') }}" class="card-img-top" alt="image">
+                                    <div class="card-body d-flex justify-content-between">
+                                        <h5 class="card-title" style="cursor: default;">Title Title Title Title Title</h5>
+                                        <p style="font-size: 12px;">Starting at - 5</p>
+                                    </div>
+                                </div>
+                            </div>
+                            @endfor
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        
     </div>
 </div>
 @push('scripts')
