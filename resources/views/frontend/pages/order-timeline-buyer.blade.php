2d9696527b2496584f3638518946ce519c32d997
Msr Nayeem <msrnayeem@gmail.com>
2023-10-17 23:53:08 +0600

    order completed

--- resources/views/frontend/pages/order-timeline-buyer.blade.php
+++ resources/views/frontend/pages/order-timeline-buyer.blade.php
@@ -23,7 +23,75 @@
             <div class="com-12">
                 <h3 class="text-center">Order Timeline For Buyer</h3>
             </div>
-            <div class="col-md-6 col-sm-12 p-3">
+            <div class="col-md-4 col-sm-12 p-3">
+
+                <div class="row justify-content-center align-items-start"
+                    style="min-height: 300px; border: 2px solid black; background:white;">
+                    @if ($StatusId == 4)
+                        <div class="col-12 mt-4">
+                            <div class="alert alert-danger" role="alert">
+                                <h4 class="alert-heading">Order Cancelled!</h4>
+                                <p>Order has been cancelled by @if (Auth::user()->name !== $lastChangedByName)
+                                        {{ $lastChangedByName }}
+                                    @else
+                                        you
+                                    @endif
+                                </p>
+
+                            </div>
+                        </div>
+                    @else
+                        <form class="col-12 mt-3" method="POST" action="{{ route('order-details.store') }}"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
+
+                            @if ($StatusId == 1)
+                                <input type="hidden" name="timeline_status_id" value="2">
+                                <div class="mb-3">
+                                    <label for="formFileMultiple" class="form-label">Send Requirements</label>
+                                    <input class="form-control" type="file" id="formFileMultiple" name="file">
+                                    <button type="submit" class="btn btn-outline-primary mt-3">Update</button>
+                                </div>
+                            @endif
+
+                            @if ($StatusId >= 1 && $StatusId <= 3)
+                                <div @class(['p-4', 'font-bold' => true]) style="background:white;">
+                                    <input type="hidden" name="timeline_status_id" value="4">
+                                    <div class="mb-3 text-center">
+                                        <p>Want to cancel the order?</p>
+                                        <div>
+                                            <button type="submit" class="btn btn-outline-primary">Cancel</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            @elseif ($StatusId == 8)
+                                <div @class(['p-4', 'font-bold' => true]) style="background:white;">
+                                    <input type="hidden" name="timeline_status_id" value="9">
+                                    <div class="mb-3 text-center">
+                                        <p>if everything is fine, then accept your order.</p>
+                                        <div>
+                                            <button type="submit" class="btn btn-outline-primary">Accept</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            @else
+                                <div class="col-12 mt-4">
+                                    <div class="alert alert-success" role="alert">
+                                        <h4 class="alert-heading">Completed</h4>
+                                        <p>Order completed 100%</p>
+
+                                    </div>
+                                </div>
+                            @endif
+
+                        </form>
+                    @endif
+                </div>
+
+            </div>
+
+            <div class="col-md-8 col-sm-12 p-3">
                 @if (session('success'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         {{ session('success') }}
@@ -68,64 +136,7 @@
                     @endforeach
                 </div>
             </div>
-            <div class="col-md-6 p-3">
-                <div class="row justify-content-center align-items-start"
-                    style="min-height: 300px; border: 2px solid black; background:white;">
-                    @if ($StatusId == 4)
-                        <div class="col-12 mt-4">
-                            <div class="alert alert-danger" role="alert">
-                                <h4 class="alert-heading">Order Cancelled!</h4>
-                                <p>Order has been cancelled by @if (Auth::user()->name !== $lastChangedByName)
-                                        {{ $lastChangedByName }}
-                                    @else
-                                        you
-                                    @endif
-                                </p>
 
-                            </div>
-                        </div>
-                    @else
-                        <form class="col-6 mt-3" method="POST" action="{{ route('order-details.store') }}"
-                            enctype="multipart/form-data">
-                            @csrf
-                            <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
-
-                            @if ($StatusId == 1)
-                                <input type="hidden" name="timeline_status_id" value="2">
-                                <div class="mb-3">
-                                    <label for="formFileMultiple" class="form-label">Send Requirements</label>
-                                    <input class="form-control" type="file" id="formFileMultiple" name="file">
-                                    <button type="submit" class="btn btn-outline-primary mt-3">Update</button>
-                                </div>
-                            @endif
-
-                            @if ($StatusId >= 1 && $StatusId <= 3)
-                                <div @class(['p-4', 'font-bold' => true]) style="background:white;">
-                                    <input type="hidden" name="timeline_status_id" value="4">
-                                    <div class="mb-3 text-center">
-                                        <p>Want to cancel the order?</p>
-                                        <div>
-                                            <button type="submit" class="btn btn-outline-primary">Cancel</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            @elseif ($StatusId == 8)
-                                <div @class(['p-4', 'font-bold' => true]) style="background:white;">
-                                    <input type="hidden" name="timeline_status_id" value="4">
-                                    <div class="mb-3 text-center">
-                                        <p>if everything is fine, then accept your order.</p>
-                                        <div>
-                                            <button type="submit" class="btn btn-outline-primary">Accept</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endif
-
-                        </form>
-                    @endif
-                </div>
-
-            </div>
 
         </div>
     </div>
--- resources/views/frontend/pages/order-timeline-seller.blade.php
+++ resources/views/frontend/pages/order-timeline-seller.blade.php
@@ -5,6 +5,22 @@
 
 @push('styles')
     <link rel="stylesheet" href="{{ asset('frontend/custom_css/order-timeline.css') }}">
+    <style>
+        .main-div {
+            display: flex;
+            position: relative;
+        }
+
+        .sticky-column {
+            position: sticky;
+            height: max-content;
+
+        }
+
+        .non-sticky {
+            height: max-content;
+        }
+    </style>
 @endpush
 
 @section('content')
@@ -18,12 +34,108 @@
     @endphp
 
 
-    <div class="container mt-4">
-        <div class="row d-flex justify-content-start mt-70 mb-70">
-            <div class="com-12">
-                <h3 class="text-center">Order Timeline For Seller</h3>
+    <div class="container mt-4 ">
+        <div class="row main-div">
+
+            <div class="col-md-4 col-sm-12 sticky-column">
+                <div class="row justify-content-center align-items-start">
+                    @if ($StatusId == 4 || $StatusId == 9)
+                        @if ($StatusId == 4)
+                            <div class="col-12 mt-4">
+                                <div class="alert alert-danger" role="alert">
+                                    <h4 class="alert-heading">Order Cancelled!</h4>
+                                    <p>Order has been cancelled by @if (Auth::user()->name !== $lastChangedByName)
+                                            {{ $lastChangedByName }}
+                                        @else
+                                            you
+                                        @endif
+                                    </p>
+
+                                </div>
+                            </div>
+                        @elseif($StatusId == 9)
+                            <div class="col-12 mt-4">
+                                <div class="alert alert-success" role="alert">
+                                    <h4 class="alert-heading">Completed</h4>
+                                    <p>Order completed 100%</p>
+
+                                </div>
+                            </div>
+                        @endif
+                    @else
+                        @if ($StatusId == 2)
+                            <form class="col-12 mt-3" method="POST" action="{{ route('order-details.store') }}"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
+
+
+                                <input type="hidden" name="timeline_status_id" value="3">
+
+                                <div @class(['p-4', 'font-bold' => true]) style="background:white;">
+                                    <div class="mb-3 text-center">
+                                        <p> received requirements from buyer?</p>
+                                        <div>
+                                            <button type="submit" class="btn btn-outline-primary">yes</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+                        @elseif($StatusId >= 1 && $StatusId <= 3)
+                            <form class="col-12 mt-3" method="POST" action="{{ route('order-details.store') }}"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
+
+                                <div class="card p-4">
+                                    <div class="form-group mb-4">
+                                        <label for="timeline_status_id">Update status now:</label>
+                                        <select id="timeline_status_id" name="timeline_status_id" class="form-control mt-2">
+                                            <option value="5">Accept</option>
+                                            <option value="4">Cancel</option>
+                                        </select>
+                                    </div>
+                                    <div class="text-end mb-2 mt-4">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        @elseif($StatusId == 5)
+                            <form class="col-12 mt-3" method="POST" action="{{ route('order-details.store') }}"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
+
+                                <div class="card p-4">
+                                    <h4> Order completed ?</h4>
+                                    <div class="form-group mb-4">
+                                        <input type="hidden" name="timeline_status_id" value="8">
+                                        <div class="mb-3">
+                                            <label for="formFileMultiple" class="form-label">Send files-</label>
+                                            <input class="form-control" type="file" id="formFileMultiple" name="file">
+                                        </div>
+                                    </div>
+                                    <div class="text-end mb-2">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        @elseif($StatusId == 8)
+                            <div class="col-12 mt-4">
+                                <div class="alert alert-success" role="alert">
+                                    <h4 class="alert-heading">Completed</h4>
+                                    <p>Now wait for the buyer to accept the order.</p>
+
+                                </div>
+                            </div>
+                        @endif
+
+                    @endif
+                </div>
+
             </div>
-            <div class="col-md-6 col-sm-12 p-3">
+            <div class="col-md-8 col-sm-12 p-3 non-sticky">
                 @if (session('success'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         {{ session('success') }}
@@ -67,95 +179,7 @@
                     @endforeach
                 </div>
             </div>
-            <div class="col-md-6 p-3">
-                <div class="row justify-content-center align-items-start"
-                    style="min-height: 300px; border: 2px solid black;">
-                    @if ($StatusId == 4 || $StatusId == 9)
-                        <div class="col-12 mt-4">
-                            <div class="alert alert-danger" role="alert">
-                                <h4 class="alert-heading">Order Cancelled!</h4>
-                                <p>Order has been cancelled by @if (Auth::user()->name !== $lastChangedByName)
-                                        {{ $lastChangedByName }}
-                                    @else
-                                        you
-                                    @endif
-                                </p>
 
-                            </div>
-                        </div>
-                    @else
-                        @if ($StatusId == 2)
-                            <form class="col-6 mt-3" method="POST" action="{{ route('order-details.store') }}"
-                                enctype="multipart/form-data">
-                                @csrf
-                                <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
-
-
-                                <input type="hidden" name="timeline_status_id" value="3">
-
-                                <div @class(['p-4', 'font-bold' => true]) style="background:white;">
-                                    <div class="mb-3 text-center">
-                                        <p> received requirements from buyer?</p>
-                                        <div>
-                                            <button type="submit" class="btn btn-outline-primary">yes</button>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </form>
-                        @elseif($StatusId >= 1 && $StatusId <= 3)
-                            <form class="col-6 mt-3" method="POST" action="{{ route('order-details.store') }}"
-                                enctype="multipart/form-data">
-                                @csrf
-                                <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
-
-                                <div class="card p-4">
-                                    <div class="form-group mb-4">
-                                        <label for="timeline_status_id">Update status now:</label>
-                                        <select id="timeline_status_id" name="timeline_status_id" class="form-control mt-2">
-                                            <option value="5">Accept</option>
-                                            <option value="4">Cancel</option>
-                                        </select>
-                                    </div>
-                                    <div class="text-end mb-2 mt-4">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </div>
-                                </div>
-                            </form>
-                        @elseif($StatusId == 5)
-                            <form class="col-6 mt-3" method="POST" action="{{ route('order-details.store') }}"
-                                enctype="multipart/form-data">
-                                @csrf
-                                <input type="hidden" name="order_id" value="{{ $order_id }}" readonly>
-
-                                <div class="card p-4">
-                                    <h4> Order completed ?</h4>
-                                    <div class="form-group mb-4">
-                                        <input type="hidden" name="timeline_status_id" value="8">
-                                        <div class="mb-3">
-                                            <label for="formFileMultiple" class="form-label">Send files-</label>
-                                            <input class="form-control" type="file" id="formFileMultiple" name="file">
-                                        </div>
-                                    </div>
-                                    <div class="text-end mb-2">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </div>
-                                </div>
-                            </form>
-                        @elseif($StatusId == 8)
-                            <div class="col-12 mt-4">
-                                <div class="alert alert-success" role="alert">
-                                    <h4 class="alert-heading">Completed</h4>
-                                    <p>Now wait for the buyer to accept the order.</p>
-
-                                </div>
-                            </div>
-                        @endif
-
-                    @endif
-                </div>
-
-            </div>
 
         </div>
     </div>
